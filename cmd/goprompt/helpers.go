1b6e1030604fe7700ea2a95c03d3c2dc54869d58
Ernie Brodeur <ebrodeur@ujami.net>
2025-02-12 11:54:02 -0800

    boom.

--- cmd/goprompt/helpers.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/spf13/cobra"
-
-// If you want to expose the root command to tests, here's a simple getter:
-func GetRootCmd() *cobra.Command {
-	return rootCmd
-}
--- internal/segments/segments.go
+++
@@ -1,6 +0,0 @@
-package segments
-
-// Example stub function.
-func Example() string {
-	return "This is a stub from package segments."
-}
