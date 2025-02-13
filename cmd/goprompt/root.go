2c052e14aeb2a233c8eda3bfc5125ad1c861f7d4
Ernie Brodeur <ebrodeur@ujami.net>
2025-02-13 15:21:38 -0800

    skeleton

---
+++ cmd/goprompt/root.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("GoPrompt - placeholder. We'll add aggregator logic soon.")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/your-username/goprompt
+
+go 1.24.0
